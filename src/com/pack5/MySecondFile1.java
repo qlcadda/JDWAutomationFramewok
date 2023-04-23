0c185c294b8606f7a565fe740fad949e0d583004
shalinik <shalinik@SHALINIK-IN.in.oracle.com>
2023-04-23 08:56:17 +0530

    added new Java File MySeconFile1.java

---
+++ src/com/pack5/MySecondFile1.java
@@ -0,0 +1,13 @@
+package com.pack5;
+
+import org.testng.annotations.Test;
+
+public class MySecondFile1 {
+
+	@Test
+	public void mySecondFileTest()
+	{
+		System.out.println("mySecondFileTest");
+	}
+
+}
